cf59d13dca653a1858c6a8727b603409177879e3
VictoryChibueze <alhajivicci12@gmail.com>
2024-11-30 13:02:24 +0100

    project-03

---
+++ controller/playerController.js
@@ -0,0 +1,20 @@
+const mongodb = require("../db/database");
+const objectId = require("mongodb").ObjectId;
+
+const getAllPlayers = () => {};
+
+const getPlayer = () => {};
+
+const createPlayer = () => {};
+
+const updatePlayer = () => {};
+
+const deletePlayer = () => {};
+
+module.exports = {
+  getAllPlayers,
+  getPlayer,
+  createPlayer,
+  updatePlayer,
+  deletePlayer,
+};
