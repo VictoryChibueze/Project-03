486bf4740e458f061cbb4437ab9997cb54b689b7
VictoryChibueze <alhajivicci12@gmail.com>
2024-11-30 14:32:02 +0100

    project-03

--- controller/playerController.js
+++ controller/playerController.js
@@ -1,15 +1,76 @@
 const mongodb = require("../db/database");
-const objectId = require("mongodb").ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 
-const getAllPlayers = () => {};
+const getAllPlayers = async (req, res) => {
+  try {
+    const result = await mongodb.getData().db().collection("Project-03").find();
+    result.toArray().then((players) => {
+      res.setHeaders("Content-Type", "application/json");
+      res.status(200).json(players);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err });
+  }
+};
 
-const getPlayer = () => {};
+const getPlayer = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getData()
+      .db()
+      .collection("Project-03")
+      .find({ _id: userId });
+    result.toArray().then((player) => {
+      res.setHeaders("Content-Type", "application/json");
+      res.status(200).json(player);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err });
+  }
+};
 
-const createPlayer = () => {};
+const createPlayer = async (req, res) => {
+  try {
+    const player = {
+      name: req.body.name,
+      gender: req.body.gender,
+      position: req.body.position,
+      clubPlayed: req.body.clubPlayed,
+      nationality: req.body.nationality,
+      birthday: req.body.birthday,
+      height: req.body.height,
+    };
 
-const updatePlayer = () => {};
+    const result = await mongodb
+      .getData()
+      .db()
+      .collection("Project-03")
+      .insertOne(player);
 
-const deletePlayer = () => {};
+    if (result.acknowledged > 0) {
+      res.status(201).send();
+    } else {
+      res
+        .status(500)
+        .json(result.error || "an error occurred while creating player");
+    }
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+const updatePlayer = () => {
+  try {
+  } catch (err) {}
+};
+
+const deletePlayer = () => {
+  try {
+  } catch (err) {}
+};
 
 module.exports = {
   getAllPlayers,
