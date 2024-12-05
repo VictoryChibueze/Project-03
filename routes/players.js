66460912fa2c4df30b6c163311419d25ec88dc87
VictoryChibueze <alhajivicci12@gmail.com>
2024-12-05 06:33:19 +0100

    project-03

--- routes/players.js
+++ routes/players.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const playerController = require("../controller/playerController");
+const validation = require("../middleware/validate");
 
 router.get("/", playerController.getAllPlayers);
 router.get("/:id", playerController.getPlayer);
-router.post("/", playerController.createPlayer);
-router.put("/:id", playerController.updatePlayer);
+router.post("/", validation.savePlayer, playerController.createPlayer);
+router.put("/:id", validation.savePlayer, playerController.updatePlayer);
 router.delete("/:id", playerController.deletePlayer);
 
 module.exports = router;
